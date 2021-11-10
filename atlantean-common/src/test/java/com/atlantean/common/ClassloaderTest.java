9290ce8bcce673def6af978780c5d9e3fcbd8941
autorun <20293349@qq.com>
2021-11-10 11:10:15 +0800

    print hashcode

--- atlantean-common/src/test/java/com/atlantean/common/ClassloaderTest.java
+++ atlantean-common/src/test/java/com/atlantean/common/ClassloaderTest.java
@@ -30,7 +30,7 @@ public class ClassloaderTest {
 		AtlanteanClassLoader classLoader = new AtlanteanClassLoader(new URL[]{new URL("com.atlantean.common.TestClass.class")});
 		Class<?> clazz = classLoader.loadClass("com.atlantean.common.TestClass.class");
 		Object o = clazz.newInstance();
-
+		System.out.println(o.hashCode());
 		Assertions.assertNotNull(clazz);
 	}
 }
