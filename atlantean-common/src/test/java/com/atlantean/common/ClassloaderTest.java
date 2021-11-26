9f70147e06cc7c4ad6138a1a8e835b83510edf7d
autorun <20293349@qq.com>
2021-11-26 11:17:43 +0800

    test

--- atlantean-common/src/test/java/com/atlantean/common/ClassloaderTest.java
+++ atlantean-common/src/test/java/com/atlantean/common/ClassloaderTest.java
@@ -31,6 +31,7 @@ public class ClassloaderTest {
 		Class<?> clazz = classLoader.loadClass("com.atlantean.common.TestClass.class");
 		Object o = clazz.newInstance();
 		System.out.println(o.getClass());
+		// sout
 		Assertions.assertNotNull(clazz);
 	}
 }
